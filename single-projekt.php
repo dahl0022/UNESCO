3c0dfe6eb5f0ef33fd564427186a0ca9117eddb1
Dahlia Rindom <dahl0022@stud.kea.dk>
2022-04-30 10:30:13 +0200

    rettelser

--- single-projekt.php
+++ single-projekt.php
@@ -1,3 +1,39 @@
+<style>
+	h1{
+		text-align: center;
+	}
+.indhold{
+	display:grid;
+	grid-template-columns: 1fr 1fr;
+	gap: 30px;
+	justify-content: center;
+	margin-right:10%;
+	margin-left:10%;
+	margin-bottom:5%;
+}
+img{
+	width: 100%;
+}
+.knap{
+	background-color: white;
+	border: solid 1.5px black;
+	border-radius: 12px;
+	margin-left: 2%;
+}
+.knap:hover{
+	border:solid 1.5px #FA9628;
+	color:black;
+}
+@media (max-width: 700px){
+	.indhold{
+	display:grid;
+	grid-template-columns: 1fr ;
+
+}
+}
+
+
+</style>
 <?php
 /**
  * The template for displaying singleview
@@ -16,11 +52,13 @@ get_header(); ?>
 <button class="knap">TILBAGE</button>
 <section class="projekt-oversigt">
 <article>
+	
 	<h1></h1>
-				<img src="" alt="" class="picture" />
+	<div class="indhold">
+	<img src="" alt="" class="picture" />
+	
 	<p></p>
-	<video class="video1" src=""></video>
-	<video class="video2" src=""></video>
+</div>
 </article>
 </section>
 
@@ -34,27 +72,31 @@ get_header(); ?>
 
 <?php endif ?>
 <script>
+	"use strict";
+	let billede = document.querySelector
 	let projekt;
 	document.addEventListener("DOMContentLoaded",getJson);
 	async function getJson(){
 		console.log("id er",<?php echo get_the_ID()?>);
-		let jsonData=await fetch (`http://dahliarindom.dk/kea/09_cms/unesco/wp-json/wp/v2/projekt/ <?php echo get_the_ID()?>`);
+		let jsonData=await fetch (`http://dahliarindom.dk/kea/09_cms/unesco/wp-json/wp/v2/projekt/<?php echo get_the_ID()?>`);
 		projekt=await jsonData.json();
 		visProjekt();
 	}
 	function visProjekt(){
-		console.log("visProjekt");
+		console.log("visProjekt",projekt);
+		
+		document.querySelector("h1").textContent=projekt.title.rendered;
                     document.querySelector(".picture").src=projekt.image.guid;
-		document.querySelector("h1").textContent=projekt.overskrift.rendered;
+		
 		document.querySelector("p").textContent=projekt.beskrivelse;
-		document.querySelector(".video1").src=projekt.video.guid;
-		document.querySelector(".video2").src=projekt.video2.guid;
 
 		document.querySelector(".knap").addEventListener("click",tilbage);
 	}
 	function tilbage(){
 		history.back();
 	}
+
+
 </script>
 <?php
 get_footer();
